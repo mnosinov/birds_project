6bb28729486a0051406b204ca7e022b19c7bad8a
mnosinov <mnosinov@gmail.com>
2022-02-07 16:41:26 +0600

    solution without form, still working

--- birds/forms.py
+++ birds/forms.py
@@ -2,11 +2,11 @@ from django.forms import ModelForm, modelformset_factory
 from .models import Bird
 
 
-class BirdForm(ModelForm):
-    class Meta:
-        model = Bird
-        fields = ['common_name', 'scientific_name']
-
+# class BirdForm(ModelForm):
+#     class Meta:
+#         model = Bird
+#         fields = ['common_name', 'scientific_name']
+# 
 
 BirdFormSet = modelformset_factory(
     Bird,
